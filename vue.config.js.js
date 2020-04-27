85bb89e54842e1ade939cfc5ad3d2452ea46a791
Qin Zhi Yong <qzy404985074@qq.com>
2020-04-27 17:29:55 +0800

    add

--- vue.config.js.js
+++ vue.config.js.js
@@ -27,7 +27,7 @@ module.exports = {
             })
     },
     /* 输出文件目录：在npm run build时，生成文件的目录名称 */
-    outputDir: 'chart-test',
+    outputDir: 'jooos-ui',
     /* 放置生成的静态资源 (mixin、css、img、fonts) 的 (相对于 outputDir 的) 目录 */
     assetsDir: "assets",
     /* 是否在构建生产包时生成 sourceMap 文件，false将提高构建速度 */
