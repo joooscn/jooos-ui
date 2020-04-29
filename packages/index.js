7de15ca2ef8f0cca47226dfb4e3640cbdcad6886
Qin Zhi Yong <qzy404985074@qq.com>
2020-04-29 10:54:07 +0800

    发布v0.1.10 JUI4page稳定版本

--- packages/index.js
+++ packages/index.js
@@ -1,9 +1,8 @@
 import JUI4page from "./page/index";
-import JUAceEditor from "./ace-editor/index";
 
 
 // 存储组件列表
-const components = [JUI4page,JUAceEditor];
+const components = [JUI4page];
 // 定义 install 方法，接收 Vue 作为参数。如果使用 use 注册插件，则所有的组件都将被注册
 const install = function (Vue) {
     // 判断是否安装
